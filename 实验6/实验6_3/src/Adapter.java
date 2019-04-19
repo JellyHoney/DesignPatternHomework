69fa4d840921d41edd9c3f1154d3a966a31c7021
Marris <494883350@qq.com>
2019-04-19 11:39:45 +0800

    实验6-3的适配器加入构造函数

--- 实验6/实验6_3/src/Adapter.java
+++ 实验6/实验6_3/src/Adapter.java
@@ -2,6 +2,11 @@ public class Adapter implements Cat,Dog{
     private Cat cat;
     private Dog dog;
 
+    public Adapter(Cat cat, Dog dog) {
+        this.cat = cat;
+        this.dog = dog;
+    }
+
     public void setDog(Dog dog) {
         this.dog = dog;
     }
--- 实验6/实验6_3/src/Client.java
+++ 实验6/实验6_3/src/Client.java
@@ -1,8 +1,6 @@
 public class Client {
     public static void main(String[] args){
-        Adapter adapter=new Adapter();
-        adapter.setCat(new ConcreteCat());
-        adapter.setDog(new ConcreteDog());
+        Adapter adapter=new Adapter(new ConcreteCat(),new ConcreteDog());
         adapter.catchMouse();
         adapter.speak();
     }
